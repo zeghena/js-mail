f29b1cceb80ca6648d4da399c52dcbfe53b96432
davidemarchini <marchini73@gmail.com>
2024-01-13 13:48:26 +0100

    check-mail, true

--- script/script.js
+++ script/script.js
@@ -1,4 +1,30 @@
-const indirizzi =  ["pippo@gmail.com" , "paperino@gmail.com" , "pluto@gmail.com"]
+
+function recuperaMail(formControl){
+    const input = document.getElementById(formControl).value;
+    return input;
+    console.log(input)
+
+}
+function checkMail(mail){
+    const indirizzi =  ["pippo@gmail.com" , "paperino@gmail.com" , "pluto@gmail.com"]
+    const test = indirizzi.includes(mail);
+    return test;
+    console.log(mail);
+}
+const btn = document.getElementById("check-mail");
+btn.addEventListener("click",function(){
+    const mail = recuperaMail("pippo");
+    const isValid = checkMail(mail);
+    console.log(isValid);
+    
+})
+
+
+
+// const result = prompt("qual è la tua mail?");
+
+
+// console.log (result)
 
 
 
